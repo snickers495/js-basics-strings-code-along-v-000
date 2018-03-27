bd74c7e2fe193096f4205049e67a8b124a020b74
Lisa Lee <snickers495@gmail.com>
2018-03-27 16:37:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your code in this file!
-const currentUser = "me"
+const currentUser = "lisa"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
 const excitedWelcomeMessage =  welcomeMessage.toUpperCase()
 const shortGreeting = `Welcome, ${currentUser[0]}!`
