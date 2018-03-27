14f304a7e95fe79f9683853f85f21579b662663b
Lisa Lee <snickers495@gmail.com>
2018-03-27 16:36:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Write your code in this file!
 const currentUser = "me"
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
-const excitedWelcomeMessage =  `Welcome to Flatbook, ${currentUser}!`
+const excitedWelcomeMessage =  welcomeMessage.toUpperCase()
